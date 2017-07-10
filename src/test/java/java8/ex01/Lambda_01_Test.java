857c16ff1f002c284d43c4f98eee4ad3f164d127
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-07-10 16:25:15 +0200

    exercices finis

--- src/test/java/java8/ex01/Lambda_01_Test.java
+++ src/test/java/java8/ex01/Lambda_01_Test.java
@@ -1,10 +1,13 @@
 package java8.ex01;
 
-import java8.data.Data;
-import java8.data.Person;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
-import java.util.List;
+import java8.data.Data;
+import java8.data.Person;
 
 
 /**
@@ -20,8 +23,16 @@ public class Lambda_01_Test {
 
     // tag::filter[]
     private List<Person> filter(List<Person> persons, PersonPredicate predicate) {
-        // TODO implementer la méthode
-        return null;
+
+    	List<Person> result = new ArrayList<Person>();
+    	
+    	for(Person p : persons) {
+    		if(predicate.test(p)) {
+    			result.add(p);
+    		}
+    	}
+    	
+    	return result;
     }
     // end::filter[]
 
@@ -33,7 +44,7 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, p -> p.getAge() >= 18);
 
         assert result.size() == 83;
 
@@ -50,7 +61,7 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, p -> p.getFirstname().equals("first_10"));
 
         assert result.size() == 1;
         assert result.get(0).getFirstname().equals("first_10");
@@ -68,7 +79,7 @@ public class Lambda_01_Test {
 
         // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+        List<Person> result = filter(personList, p -> DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex) && p.getAge() > 49);
 
         assert result.size() == 6;
         for (Person person : result) {
--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -1,11 +1,13 @@
 package java8.ex02;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
 import java8.data.Account;
 import java8.data.Data;
 import java8.data.Person;
-import org.junit.Test;
-
-import java.util.List;
 
 
 /**
@@ -14,15 +16,22 @@ import java.util.List;
 public class Lambda_02_Test {
 
     // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
-        Account map(Person p);
+    interface PersonToAccountMapper<T> { // On pourrait faire un type générique pour le "Person p" également
+        T map(Person p);
     }
     // end::PersonToAccountMapper[]
 
     // tag::map[]
-    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
+    private <T> List<T> map(List<Person> personList, PersonToAccountMapper<T> mapper) {
         // TODO implémenter la méthode pour transformer une liste de personnes en liste de comptes
-        return null;
+    	
+    	List<T> listAccout = new ArrayList<>();
+    	
+    	for(Person p : personList) {
+    		listAccout.add(mapper.map(p));
+    	}
+    	
+        return listAccout;
     }
     // end::map[]
 
@@ -35,7 +44,8 @@ public class Lambda_02_Test {
 
         // TODO transformer la liste de personnes en liste de comptes
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+        
+        List<Account> result = map(personList, p -> {Account a = new Account(); a.setOwner(p); a.setBalance(100); return a;});
 
         assert result.size() == personList.size();
         for (Account account : result) {
@@ -52,7 +62,7 @@ public class Lambda_02_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+        List<String> result = map(personList, n -> n.getFirstname());
 
         assert result.size() == personList.size();
         for (String firstname : result) {
